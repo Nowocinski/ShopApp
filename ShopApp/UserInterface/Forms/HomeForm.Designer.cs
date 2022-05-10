c86eb26c0b24d2f0d19280a1a63e74985009e5e1
Nowocinski <vito24222@gmail.com>
2022-05-10 22:08:31 +0200

    Added of forms home and login

---
+++ ShopApp/UserInterface/Forms/HomeForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace UserInterface.Forms
+{
+    partial class HomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loggingButton = new System.Windows.Forms.Button();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loggingButton
+            // 
+            this.loggingButton.Location = new System.Drawing.Point(182, 255);
+            this.loggingButton.Name = "loggingButton";
+            this.loggingButton.Size = new System.Drawing.Size(94, 29);
+            this.loggingButton.TabIndex = 0;
+            this.loggingButton.Text = "Zaloguj";
+            this.loggingButton.UseVisualStyleBackColor = true;
+            // 
+            // signupButton
+            // 
+            this.signupButton.Location = new System.Drawing.Point(383, 255);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(94, 29);
+            this.signupButton.TabIndex = 1;
+            this.signupButton.Text = "Zarejestruj";
+            this.signupButton.UseVisualStyleBackColor = true;
+            // 
+            // HomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.loggingButton);
+            this.Name = "HomeForm";
+            this.Text = "HomeForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button loggingButton;
+        private Button signupButton;
+    }
+}
---
+++ ShopApp/UserInterface/Forms/LoginForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace UserInterface.Forms
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signupButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // signupButton
+            // 
+            this.signupButton.Location = new System.Drawing.Point(423, 350);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(148, 50);
+            this.signupButton.TabIndex = 0;
+            this.signupButton.Text = "Nie masz konta? Zarejestruj się";
+            this.signupButton.UseVisualStyleBackColor = true;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(630, 361);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(94, 29);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Powrót";
+            this.backButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.signupButton);
+            this.Name = "LoginForm";
+            this.Text = "LoginForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button signupButton;
+        private Button backButton;
+    }
+}
