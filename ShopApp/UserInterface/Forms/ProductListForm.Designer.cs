04e50aeb8885a13fd7bb8c859984cb5db73d8f29
Nowocinski <vito24222@gmail.com>
2022-05-28 11:28:38 +0200

    Added retrieval of product data

--- ShopApp/UserInterface/Forms/ProductListForm.Designer.cs
+++ ShopApp/UserInterface/Forms/ProductListForm.Designer.cs
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.prooductsDataGridView = new System.Windows.Forms.DataGridView();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.prooductsDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(303, 67);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(115, 20);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Lista produktów";
+            // 
+            // prooductsDataGridView
+            // 
+            this.prooductsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.prooductsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Price});
+            this.prooductsDataGridView.Location = new System.Drawing.Point(48, 108);
+            this.prooductsDataGridView.Name = "prooductsDataGridView";
+            this.prooductsDataGridView.RowHeadersWidth = 51;
+            this.prooductsDataGridView.RowTemplate.Height = 29;
+            this.prooductsDataGridView.Size = new System.Drawing.Size(498, 211);
+            this.prooductsDataGridView.TabIndex = 1;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Nazwa";
+            this.Name.MinimumWidth = 6;
+            this.Name.Name = "Name";
+            this.Name.Width = 125;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Cena";
+            this.Price.MinimumWidth = 6;
+            this.Price.Name = "Price";
+            this.Price.Width = 125;
+            // 
+            // ProductListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.prooductsDataGridView);
+            this.Controls.Add(this.titleLabel);
+            this.Name.Name = "ProductListForm"; // TODO: samo this.Name nie działało
             this.Text = "ProductListForm";
+            this.Load += new System.EventHandler(this.ProductListForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.prooductsDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label titleLabel;
+        private DataGridView prooductsDataGridView;
+        private DataGridViewTextBoxColumn Name;
+        private DataGridViewTextBoxColumn Price;
     }
 }
--- ShopApp/UserInterface/Forms/ProductListForm.cs
+++ ShopApp/UserInterface/Forms/ProductListForm.cs
@@ -1,10 +1,22 @@
-﻿namespace UserInterface.Forms
+﻿using DataAccessLayer.Repositories;
+using DataAccessLayer.ViewModel;
+
+namespace UserInterface.Forms
 {
     public partial class ProductListForm : Form
     {
+        private readonly IProductRepository productRepository;
         public ProductListForm()
         {
             InitializeComponent();
+
+            this. productRepository = new ProductRepository();
+        }
+
+        private async void ProductListForm_Load(object sender, EventArgs e)
+        {
+            IEnumerable<ProductViewModel> products = await productRepository.GetAllAsync();
+            throw new NotImplementedException();
         }
     }
 }
